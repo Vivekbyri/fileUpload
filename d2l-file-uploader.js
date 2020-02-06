8b66f54348c176e6cab0ef9a2412ef387fad5109
Vivekbyri <vivek.byri@springml.com>
2020-02-06 11:06:11 +0530

    update package

--- d2l-file-uploader.js
+++ d2l-file-uploader.js
@@ -11,8 +11,8 @@
 //import '@polymer/polymer/polymer-legacy.js';
 
 //import '@brightspace-ui/core/components/colors/colors.js';
-import 'd2l-polymer-behaviors/d2l-focusable-behavior.js';
-import 'd2l-offscreen/d2l-offscreen-shared-styles.js';
+// import 'd2l-polymer-behaviors/d2l-focusable-behavior.js';
+// import 'd2l-offscreen/d2l-offscreen-shared-styles.js';
 import './localize-behavior.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 const $_documentContainer = document.createElement('template');
